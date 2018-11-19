bcbc8b602d03093d371d5b583f81a4da847c75f6
suwa <suwa@techmatrix.co.jp>
2018-11-19 19:22:57 +0900

    www

--- sqlInjection.c
+++ sqlInjection.c
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-#include "sql.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define BUF_SIZE 1000
-
-const char* requestBeginning = "SELECT user_id, user_class, rights FROM users WHERE user_name = '";
-const char* requestPassword = "' and password = '";
-const char* requestEnding = "'";
-
-char* extractUsername(char* s)
-{
-	/* Some implementation */
-	int a=0;
-	return s;
-}
-
-char* extractPassword(char* s)
-{
-	/* Some implementation */
-	return s;
-}
-
-void handleRequest(SQLHSTMT statementHandle, FILE *file)
-{
-    char parametersString[BUF_SIZE];
-    char request[BUF_SIZE];
-
-    fread(parametersString, 1, BUF_SIZE, file);
-    strcpy(request, requestBeginning);
-    strcat(request, extractUsername(parametersString));
-    strcat(request, requestPassword);
-    strcat(request, extractPassword(parametersString));
-    strcat(request, requestEnding);
-    SQLExecDirect(statementHandle, (SQLCHAR *)request, strlen(request));
-}
-
-=======
 #include "sql.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -74,5 +34,3 @@ void handleRequest(SQLHSTMT statementHandle, FILE *file)
     strcat(request, requestEnding);
     SQLExecDirect(statementHandle, (SQLCHAR *)request, strlen(request));
 }
-
->>>>>>> branch 'master' of https://github.com/tmx-se/redmine-demo.git
